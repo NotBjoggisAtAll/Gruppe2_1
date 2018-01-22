706e6d133fc72590b3c960d5b7a7c467ae8ab570
Jonas BA <jonas.bjorge.andersen@gmail.com>
2018-01-22 16:32:32 +0100

    Changed projectile code from blueprint to c++

--- Source/Gruppe2_1/Projectile.h
+++ Source/Gruppe2_1/Projectile.h
@@ -6,22 +6,34 @@
 #include "GameFramework/Actor.h"
 #include "Projectile.generated.h"
 
+class UProjectileMovementComponent;
+class UStaticMeshComponent;
+
 UCLASS()
 class GRUPPE2_1_API AProjectile : public AActor
 {
 	GENERATED_BODY()
-	
+
+	/** Sphere collision component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Projectile, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* ProjectileMesh;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+
 public:	
 	// Sets default values for this actor's properties
 	AProjectile();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	/** Function to handle the projectile hitting something */
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	/** Returns ProjectileMesh subobject **/
+	FORCEINLINE UStaticMeshComponent* GetProjectileMesh() const { return ProjectileMesh; }
+	/** Returns ProjectileMovement subobject **/
+	FORCEINLINE UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
 
 	
 	
