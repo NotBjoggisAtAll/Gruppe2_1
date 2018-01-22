a2e116a782ca11e858d0ce2ceca34a82a2a04945
Bjoggis <jonas.bjorge.andersen@gmail.com>
2018-01-22 11:58:39 +0100

    Created Movement in C++, need to add jump and shoot.

--- Source/Gruppe2_1/MyCharacter.cpp
+++ Source/Gruppe2_1/MyCharacter.cpp
@@ -1,14 +1,40 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Sjekk hvilke includes jeg faktisk bruker
 #include "MyCharacter.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
 
+//const FName AMyCharacter::MoveForwardBinding("MoveForward");
+//const FName AMyCharacter::MoveRightBinding("MoveRight");
+//const FName AMyCharacter::TurnBinding("Turn");
 
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Camera boom
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 500.f;
+	CameraBoom->RelativeRotation = FRotator(-45.f, 130.f, 0.f);
+	CameraBoom->bUsePawnControlRotation = false;
+
+	// Create Camera
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	CameraComponent->bUsePawnControlRotation = false;
 }
 
 // Called when the game starts or when spawned
@@ -21,14 +47,55 @@ void AMyCharacter::BeginPlay()
 // Called every frame
 void AMyCharacter::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
+	//const float ForwardValue = GetInputAxisKeyValue(MoveForwardBinding);
+	//const float RightValue = GetInputAxisKeyValue(MoveRightBinding);
 
+	//Clamp movement
+	//const FVector MoveDirection = FVector(ForwardValue, RightValue, 0.f).GetClampedToMaxSize(1.f);
+
+	////Calculate movement
+	//const FVector Movement = MoveDirection * MoveSpeed * DeltaTime;
+
+
+	//if (Movement.SizeSquared() > 0.0f)
+	//{
+	//	const FRotator NewRotation = Movement.Rotation();
+	//	FHitResult Hit(1.f);
+	//	RootComponent->MoveComponent(Movement, NewRotation, true, &Hit);
+
+	//	if (Hit.IsValidBlockingHit())
+	//	{
+	//		const FVector Normal2D = Hit.Normal.GetSafeNormal2D();
+	//		const FVector Deflection = FVector::VectorPlaneProject(Movement, Normal2D) * (1.f - Hit.Time);
+	//		RootComponent->MoveComponent(Deflection, NewRotation, true);
+	//	}
+	//}
+}
+
+void AMyCharacter::MoveForward(float Value) {
+	if (Value != 0.f) {
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void AMyCharacter::MoveRight(float Value) {
+	if (Value != 0.f) {
+		AddMovementInput(GetActorRightVector(), Value);
+	}
 }
 
 // Called to bind functionality to input
 void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
 
+	//Set up key bindings
+	PlayerInputComponent->BindAxis("MoveForward", this , &AMyCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
+//	PlayerInputComponent->BindAxis(TurnBinding);
+
+	//const FName AMyCharacter::MoveForwardBinding("MoveForward");
+	//const FName AMyCharacter::MoveRightBinding("MoveRight");
+	//const FName AMyCharacter::TurnBinding("Turn");
 }
 
--- Source/Gruppe2_1/MyCharacter.h
+++ Source/Gruppe2_1/MyCharacter.h
@@ -11,13 +11,33 @@ class GRUPPE2_1_API AMyCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* CameraComponent;
+
+	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
 public:
 	// Sets default values for this character's properties
 	AMyCharacter();
 
+
+
+	// Static names for axis bindings
+	/*static const FName MoveForwardBinding;
+	static const FName MoveRightBinding;
+	static const FName TurnBinding;*/
+	//static const FName FireRightBinding;
+
+	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
+		float MoveSpeed;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
 
 public:	
 	// Called every frame
